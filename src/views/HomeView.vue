b787dbb36051d993b34ad58c4045fe8add3a162c
katie oconnor <katina.surise@gmail.com>
2025-09-17 21:51:38 -0500

    GPS2-3 - game controller and test page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    hello world
+    <GameControllerTest />
   </div>
 </template>
 
 <script setup lang="ts">
+import GameControllerTest from '../components/GameControllerTest.vue'
 </script>
